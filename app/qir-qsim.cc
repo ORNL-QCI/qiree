ffea95eb48f737cd8587ceabe691df4c1a47bd15
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2025-03-11 14:03:00 -0400

    Removed code duplication

--- app/qir-qsim.cc
+++ app/qir-qsim.cc
@@ -18,34 +18,8 @@
 
 using namespace std::string_view_literals;
 
-namespace qiree
-{
-namespace app
-{
+int parse_input(int argc, char* argv[]);
 //---------------------------------------------------------------------------//
-void run(std::string const& filename, int num_shots)
-{
-    // Load the input
-    Executor execute{Module{filename}};
-
-    // Set up qsim
-    QsimQuantum sim(std::cout, 0);
-    QsimDefaultRuntime rt(std::cout, sim);
-    ResultDistribution distribution;
-
-    // Run several time = shots (default 1)
-    for (int i = 0; i < num_shots; i++)
-    {
-        execute(sim, rt);
-        distribution.accumulate(rt.result());
-    }
-
-    std::cout << distribution.to_json() << std::endl;
-}
-
-//---------------------------------------------------------------------------//
-}  // namespace app
-}  // namespace qiree
 
 //---------------------------------------------------------------------------//
 /*!
@@ -53,22 +27,5 @@ void run(std::string const& filename, int num_shots)
  */
 int main(int argc, char* argv[])
 {
-    int num_shots{1};
-    std::string filename;
-
-    CLI::App app;
-
-    auto* filename_opt
-        = app.add_option("--input,-i,input", filename, "QIR input file");
-    filename_opt->required();
-
-    auto* nshot_opt
-        = app.add_option("-s,--shots", num_shots, "Number of shots");
-    nshot_opt->capture_default_str();
-
-    CLI11_PARSE(app, argc, argv);
-
-    qiree::app::run(filename, num_shots);
-
-    return EXIT_SUCCESS;
+    return parse_input(argc, argv);
 }
